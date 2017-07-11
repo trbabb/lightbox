e32ce35e692f4659d77382b818a1bdb3465f9cf4
Tim Babb <trbabb@gmail.com>
2017-07-11 10:43:01 -0400

    factor into nice functions

--- src/Image.h
+++ src/Image.h
@@ -15,6 +15,11 @@
 
 
 using namespace geom;
+
+
+// forward declaration:
+template <typename T, index_t Channels>
+class Image;
  
 
 // clip `val` to lie between `lo` and `hi`.
@@ -22,50 +27,6 @@ template <typename T>
 inline T clamp(T val, T lo, T hi) {
     return std::max(std::min(val, hi), lo);
 }
-
-
-// class holding a 2D image, with channel values of type T.
-template <typename T, index_t Channels>
-class Image {
-    public:
-    
-    // defines a type which is Vec<T,Channels>, unless Channels == 1 (then it's simply a T).
-    typedef typename PointType<T,Channels>::point_t pixel;
-    
-    pixel* data;
-    index_t width;
-    index_t height;
-    
-    
-    Image(index_t w, index_t h):
-            data(new pixel[w * h]),
-            width(w),
-            height(h) {}
-    
-    
-    // get the pixel at the given coordinates
-    inline pixel& operator[](Vec2i coords) {
-        return data[coords.y * width + coords.x];
-    }
-    
-    
-    inline pixel operator[](Vec2i coords) const {
-        return data[coords.y * width + coords.x];
-    }
-    
-    
-    // get the width and height of the image
-    inline Vec2i dimensions() const {
-        return Vec2i(width,height);
-    }
-    
-    
-    // get the number of channels in the image
-    inline index_t channels() const {
-        return Channels;
-    }
-    
-};
 
 
 // get the raw data bytes to write to file.
@@ -116,61 +77,112 @@ png_bytep* png_data(
 }
 
 
-// save an image to a PNG file.
+// class holding a 2D image, with channel values of type T.
 template <typename T, index_t Channels>
-bool save_png(const Image<T,Channels> &img, // Image data to write out
-              const char *filepath,         // Where to save the image
-              double exposure=0.,           // Exposure parameter, in camera stops. Negative = darker; positive = brighter
-              bool sixteenbit=false) {      // Whether to save as a 16 bit PNG.
-    int imgtype;
-    switch (img.channels()){
-        case 1:
-            imgtype = PNG_COLOR_TYPE_GRAY;
-            break;
-        case 2:
-            imgtype = PNG_COLOR_TYPE_GRAY_ALPHA;
-            break;
-        case 3:
-            imgtype = PNG_COLOR_TYPE_RGB;
-            break;
-        case 4:
-            imgtype = PNG_COLOR_TYPE_RGB_ALPHA;
-            break;
-        default:
-            return false;
+class Image {
+    public:
+    
+    // defines a type which is Vec<T,Channels>, unless Channels == 1 (then it's simply a T).
+    typedef typename PointType<T,Channels>::point_t pixel;
+    
+    pixel*  data;
+    index_t width;
+    index_t height;
+    
+    
+    // constructor
+    Image(index_t w, index_t h):
+            data(new pixel[w * h]),
+            width(w),
+            height(h) {}
+    
+    
+    // destructor
+    ~Image() {
+        delete [] data;
+        data   = NULL;
+        width  = 0;
+        height = 0;
     }
     
-    index_t w = img.dimensions()[0];
-    index_t h = img.dimensions()[1];
     
-    // white point
-    T scale = std::pow(2, exposure);
-    typename PointType<T,Channels>::point_t white = typename PointType<T,Channels>::point_t(scale);
+    // get the pixel at the given coordinates
+    inline pixel& operator[](Vec2i coords) {
+        return data[coords.y * width + coords.x];
+    }
     
-    // get raw pixel data (bytes) for writing
-    png_bytep *imgdat = png_data<T,Channels>(img, white, sixteenbit);
     
-    // actually do the write.
-    FILE *fp = fopen(filepath, "wb");
-    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (!png_ptr) throw std::runtime_error("could not make png write structure");
-    png_infop  info_ptr = png_create_info_struct(png_ptr);
-    if (!info_ptr) throw std::runtime_error("could not make png info structure");
-    if (setjmp(png_jmpbuf(png_ptr))) throw std::runtime_error("png write init failure");
-    png_init_io(png_ptr, fp);
-    if (setjmp(png_jmpbuf(png_ptr))) throw std::runtime_error("png write header failure");
-    png_set_IHDR(png_ptr, info_ptr, w, h, sixteenbit?16:8, imgtype, PNG_INTERLACE_NONE,
-            PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
-    //png_set_gAMA(png_ptr, info_ptr, 1/2.2);
-    png_set_sRGB(png_ptr, info_ptr, PNG_sRGB_INTENT_ABSOLUTE);
-    png_write_info(png_ptr, info_ptr);
-    if (setjmp(png_jmpbuf(png_ptr))) throw std::runtime_error("png write image failure");
-    png_write_image(png_ptr, imgdat);
-    if (setjmp(png_jmpbuf(png_ptr))) throw std::runtime_error("png write completion failure");
-    png_write_end(png_ptr, NULL);
-    fclose(fp);
-    return true;
-}
+    inline pixel operator[](Vec2i coords) const {
+        return data[coords.y * width + coords.x];
+    }
+    
+    
+    // get the width and height of the image
+    inline Vec2i dimensions() const {
+        return Vec2i(width,height);
+    }
+    
+    
+    // get the number of channels in the image
+    inline index_t channels() const {
+        return Channels;
+    }
+    
+    
+    // save this image to PNG file.
+    inline bool save_png(
+            const char* filepath,          // Where to save the image
+            double exposure=0,             // Exposure parameter, in camera stops. Negative = darker; positive = brighter
+            bool sixteenbit=false) const { // Whether to save with 16-bit pixel data.
+        int imgtype;
+        switch (Channels){
+            case 1:
+                imgtype = PNG_COLOR_TYPE_GRAY;
+                break;
+            case 2:
+                imgtype = PNG_COLOR_TYPE_GRAY_ALPHA;
+                break;
+            case 3:
+                imgtype = PNG_COLOR_TYPE_RGB;
+                break;
+            case 4:
+                imgtype = PNG_COLOR_TYPE_RGB_ALPHA;
+                break;
+            default:
+                return false;
+        }
+        
+        // white point
+        T scale = std::pow(2, exposure);
+        typename PointType<T,Channels>::point_t white = typename PointType<T,Channels>::point_t(scale);
+        
+        // get raw pixel data (bytes) for writing
+        png_bytep *imgdat = png_data<T,Channels>(*this, white, sixteenbit);
+        
+        // actually do the write.
+        FILE *fp = fopen(filepath, "wb");
+        png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+        if (!png_ptr) throw std::runtime_error("could not make png write structure");
+        png_infop  info_ptr = png_create_info_struct(png_ptr);
+        if (!info_ptr) throw std::runtime_error("could not make png info structure");
+        if (setjmp(png_jmpbuf(png_ptr))) throw std::runtime_error("png write init failure");
+        png_init_io(png_ptr, fp);
+        if (setjmp(png_jmpbuf(png_ptr))) throw std::runtime_error("png write header failure");
+        png_set_IHDR(png_ptr, info_ptr, width, height, sixteenbit?16:8, imgtype, PNG_INTERLACE_NONE,
+                PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+        //png_set_gAMA(png_ptr, info_ptr, 1/2.2);
+        png_set_sRGB(png_ptr, info_ptr, PNG_sRGB_INTENT_ABSOLUTE);
+        png_write_info(png_ptr, info_ptr);
+        if (setjmp(png_jmpbuf(png_ptr))) throw std::runtime_error("png write image failure");
+        png_write_image(png_ptr, imgdat);
+        if (setjmp(png_jmpbuf(png_ptr))) throw std::runtime_error("png write completion failure");
+        png_write_end(png_ptr, NULL);
+        fclose(fp);
+        
+        return true;
+    }
+    
+};
 
 
 #endif /* IMAGE_H_ */
--- src/lightbox.cpp
+++ src/lightbox.cpp
@@ -7,20 +7,40 @@
 using namespace geom;
 using namespace std;
 
-int main(int argc, char** argv) {
-    Image<double,3> img(1024, 512); // a 3-channel image.
+
+// Produce a pixel color based on the image coordinates x,y.
+// The image center will be (0,0), and x will range from -0.5 to 0.5.
+inline Vec3d render_pixel(Vec2d xy) {
+    double phz = xy.x * M_PI * 12;
     
+    // here's some expression which makes a pixel color value:
+    Vec3d px = Vec3d(
+        cos(phz)/2 + 0.5,      // red
+        cos(phz*1.1)/2 + 0.5,  // green
+        cos(phz*1.2)/2 + 0.5); // blue
+    
+    return px;
+}
+
+
+void render_image(Image<double,3>& img) {
     for (index_t y = 0; y < img.height; ++y) {
         for (index_t x = 0; x < img.width; ++x) {
             Vec2i xy = Vec2i(x,y);
             Vec2d st = ((Vec2d)(xy)) / img.width;
             st -= Vec2d(0.5, (0.5 * img.height) / img.width);
-            double phz = st.x * M_PI * 12;
-            img[xy] = Vec3d(cos(phz)/2 + 0.5, cos(phz*1.08123123)/2 + 0.5, cos(phz*1.1912312)/2 + 0.5);
+            img[xy] = render_pixel(st);
         }
     }
+}
+
+
+int main(int argc, char** argv) {
+    Image<double,3> img(1024, 512); // a 3-channel image.
     
-    save_png(img, "output/test.png");
+    render_image(img);
+    
+    img.save_png("output/test.png");
     
     return 0;
 }
